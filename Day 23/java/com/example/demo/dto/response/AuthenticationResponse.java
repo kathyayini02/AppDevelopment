e98b65597f3645836afc98eb44ed5628bfd6850f
kathyayini02 <120082908+kathyayini02@users.noreply.github.com>
2023-10-16 12:20:12 +0530

    Add files via upload

---
+++ Day 23/java/com/example/demo/dto/response/AuthenticationResponse.java
@@ -0,0 +1,16 @@
+package com.example.demo.dto.response;
+
+
+
+
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse 
+{
+	private String token;
+
+}
